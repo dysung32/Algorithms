1cbfd1ed72ad92fa1d9e6ed5a0426678a8fcc8b6
yoni <sdy32@naver.com>
2022-04-19 23:35:49 +0900

    baekjoon 요세푸스

--- baekjoon/baekjoon_1158.py
+++ baekjoon/baekjoon_1158.py
@@ -5,4 +5,15 @@
 import sys
 
 N, K = map(int, sys.stdin.readline().split())
-print(N, K)
+arr = [i for i in range(1, N + 1)]
+
+answer = []
+num = 0
+
+for j in range(N):
+    num += K - 1
+    if num >= len(arr):
+        num = num % len(arr)
+    answer.append(str(arr.pop(num)))
+
+print("<", ", ".join(answer), ">", sep='')
