0ba14e6591c027b3476216afa7be5cfa5c51840f
yoni <sdy32@naver.com>
2022-05-25 09:18:59 +0900

    20_getSquaredElementsAtProperty

---
+++ others/20_getSquaredElementsAtProperty.js
@@ -0,0 +1,11 @@
+function square(number) {
+  return number * number;
+}
+
+function getSquaredElementsAtProperty(obj, property) {
+  let value = obj[property]
+  if (Array.isArray(value) === true) {
+    return value.map(square)
+  }
+  return []
+}
