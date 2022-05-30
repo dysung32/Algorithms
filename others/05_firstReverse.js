779d947c08ccfd4b6d541202a861b0c130cc0cd8
yoni <sdy32@naver.com>
2022-05-30 14:19:16 +0900

    05_firstReverse

--- others/05_firstReverse.js
+++ others/05_firstReverse.js
@@ -1,11 +1,11 @@
 function firstReverse(str) {
-    const arr = [...str]
+  const arr = [...str]
 
-    let arr_reversed = arr.reverse()
+  let arr_reversed = arr.reverse()
 
-    let str_reversed = ''
-    for (let i = 0; i < arr_reversed.length; i++) {
-      str_reversed += arr_reversed[i]
-    }
-    return str_reversed
+  let str_reversed = ''
+  for (let i = 0; i < arr_reversed.length; i++) {
+    str_reversed += arr_reversed[i]
+  }
+  return str_reversed
 }
