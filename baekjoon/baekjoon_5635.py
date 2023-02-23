f685160e2f0b9c786d99f363156e67c05dbfcc23
Da Yeon Sung <sdy32@naver.com>
2023-02-23 17:56:47 +0900

    Baekjoon_5635_생일

--- baekjoon/baekjoon_5635.py
+++ baekjoon/baekjoon_5635.py
@@ -2,4 +2,12 @@ import sys
 
 N = int(sys.stdin.readline())
 
-print(N)
+list = []
+
+for _ in range(N):
+    name, day, month, year = input().split()
+    list.append([name, int(day), int(month), int(year)])
+
+list.sort(key=lambda x:(x[3], x[2], x[1]))
+print(list[-1][0]) # 가장 나이 적은 사람 (year로는 가장 큼)
+print(list[0][0]) # 가장 나이 많은 사람
