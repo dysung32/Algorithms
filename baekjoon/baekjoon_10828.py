dbc1e70fc9d4e091d32efb094170a7f488e3d450
Da Yeon Sung <sdy32@naver.com>
2023-05-31 17:19:45 +0900

    Baekjoon_10828_스택

--- baekjoon/baekjoon_10828.py
+++ baekjoon/baekjoon_10828.py
@@ -1,32 +1,28 @@
-# 스택
 import sys
+input = sys.stdin.readline
 
-N = int(sys.stdin.readline())
+N = int(input())
 stack = []
 
-for i in range(N):
-    cmd = sys.stdin.readline().split()
-    # push 가 입력 되면
+for _ in range(N):
+    cmd = input().split()
+
     if cmd[0] == 'push':
         stack.append(cmd[1])
-    # pop 이 입력 되면
     elif cmd[0] == 'pop':
-        if len(stack) == 0:
-            print(-1)
-        else:
+        if stack:
             print(stack.pop())
-    # size 가 입력 되면
+        else:
+            print(-1)
     elif cmd[0] == 'size':
         print(len(stack))
-    # empty 가 입력 되면
     elif cmd[0] == 'empty':
-        if len(stack) == 0:
-            print(1)
-        else:
+        if stack:
             print(0)
-    # top 이 입력 되면
-    elif cmd[0] == 'top':
-        if len(stack) == 0:
-            print(-1)
         else:
+            print(1)
+    elif cmd[0] == 'top':
+        if stack:
             print(stack[-1])
+        else:
+            print(-1)
