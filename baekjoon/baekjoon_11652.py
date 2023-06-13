1f4c582894898141bc3cb4c7d9bc99078ce15bc5
Da Yeon Sung <sdy32@naver.com>
2023-06-14 05:03:20 +0900

    Baekjoon_11652_카드

---
+++ baekjoon/baekjoon_11652.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+cards = {}
+for _ in range(N):
+    card = int(input())
+    if card in cards:
+        cards[card] += 1
+    else:
+        cards[card] = 1
+
+ans = sorted(cards.items(), key=lambda x: (-x[1], x[0]))
+
+print(ans[0][0])
