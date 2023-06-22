8ba0df0de23940b98e698af3135330f7130990fa
Da Yeon Sung <sdy32@naver.com>
2023-06-22 10:30:02 +0900

    Baekjoon_15652_N과 M(4)

---
+++ baekjoon/baekjoon_15652.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+nums = []
+
+def dfs():
+    if len(nums) == M:
+        if nums == sorted(nums):
+            print(*nums)
+        return
+    for i in range(1, N + 1):
+        nums.append(i)
+        dfs()
+        nums.pop()
+
+dfs()
