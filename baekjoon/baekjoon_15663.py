72c4fab12dfbe9973ddb5ab75489c316cf8e380f
Da Yeon Sung <sdy32@naver.com>
2023-06-28 17:08:02 +0900

    Baekjoon_15663_N과 M (9)

---
+++ baekjoon/baekjoon_15663.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+nums = sorted(list(map(int, input().split())))
+visited = [False] * N
+
+arr = []
+
+def dfs():
+    if len(arr) == M:
+        print(*arr)
+        return
+    last_one = 0
+    for i in range(N):
+        if not visited[i] and last_one != nums[i]:
+            visited[i] = True
+            arr.append(nums[i])
+            last_one = nums[i]
+            dfs()
+            visited[i] = False
+            arr.pop()
+
+dfs()
